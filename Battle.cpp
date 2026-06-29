804edd75a624229860421a4ccad116f5fb5f7b5d
Jayant <saigee2703@gmail.com>
2026-06-29 15:58:09 +0530

    Refactor combat system into Battle module

---
+++ Battle.cpp
@@ -0,0 +1,87 @@
+#include "Battle.h"
+#include <iostream>
+int date=1;
+int MobCount=0;
+
+void DaySurvived(){
+    std::cout<<"Days survived: "<<date<<std::endl;
+}
+
+
+
+void MobKilling(){
+    std::cout<<"Monsters finished: "<<MobCount<<std::endl;
+}
+
+
+void Battle(Player & hero, Enemy & enemy){
+    int b;
+    while(hero.GetHealth()>0 && enemy.GetHealth()>0){
+        std::cout<<"\n";
+        std::cout<<"......................."<<std::endl;
+        std::cout<<"1. ATTACK ENEMY"<<std::endl;
+        std::cout<<"2. SHOW STATS"<<std::endl;
+        std::cout<<"3. EXIT"<<std::endl;
+        std::cout<<"\n";
+        std::cout<<"Choose one option: ";
+        std::cin>>b;
+        std::cout<<"\n";
+
+    switch(b){
+        case 1:{
+        hero.Attack();
+        enemy.TakeDamage(hero.GetStrength());
+        std::cout<< enemy.GetName()<<" Health: "<<enemy.GetHealth()<<std::endl;
+        std::cout<<"\n";
+
+        if(enemy.GetHealth()<=0)
+        {enemy.Fatality(hero.GetName());
+        std::cout<<enemy.GetName()<<" is defeated!"<<std::endl;
+        std::cout<<"\nBattle Won Successfully!"<<std::endl;
+        std::cout << "\n=========================================\n";
+        MobCount++;
+        break;
+        }
+
+        enemy.Attack();
+        hero.TakeDamage(enemy.GetDamage());
+        std::cout<<hero.GetName()<<" Health: "<<hero.GetHealth()<<std::endl;
+        std::cout<<"\n";
+        break;
+        }
+
+        case 2:{
+            hero.show();
+            std::cout<<"\n";
+            enemy.show();
+            break;
+        }
+
+        case 3:
+        {
+        std::cout<<hero.GetName()<<" ran from the ground!!"<<std::endl;
+        std::cout<<"\n";
+        return;
+        }
+        
+        default:{
+            std::cout<<"Wrong move!!"<<std::endl;
+            break;
+        }
+
+    }
+    }
+    if(hero.GetHealth()<=0){
+        std::cout<<enemy.GetName()<<" gobbles up the bones of "<<hero.GetName()<<" alive..!\n";
+        std::cout<<hero.GetName()<<" was devoured by "<<enemy.GetName()<<std::endl;
+        std::cout<<hero.GetName()<<" was killed by "<<enemy.GetName()<<"!"<<std::endl;
+        std::cout<<"GAME OVER..."<<std::endl;
+        std::cout << "\n=========================================\n\n";
+        std::cout<<"Player History: \n";
+        hero.show();
+        MobKilling();
+        DaySurvived();
+        std::cout << "\n=========================================\n";
+    }
+    
+}
---
+++ Battle.h
@@ -0,0 +1,9 @@
+#ifndef BATTLE_H
+#define BATTLE_H
+#include "Player.h"
+#include "Enemy.h"
+
+void Battle(Player & hero, Enemy & enemy);
+void MobKilling();
+void DaySurvived();
+#endif
